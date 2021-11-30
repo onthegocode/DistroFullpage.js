af212d239c79fc08257ef92bf7fe13f358590af5
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2021-11-29 17:51:45 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// FullPage.js
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors:['section1', 'section2', 'section3', 'footer'],
+    navigationTooltips: ['About Us', 'Our Goals', 'Get in Touch'],
+    scrollingSpeed: 1250,
+    sectionSelector: 'section',
+    scrollBar: true,
+    controlArrows: false,
+    verticalCentered: false
+})
+
+// Copyright Update new year
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+
+// Animation
+
+    const getintouch = document.querySelector('#footer-cta');
+    const submit = document.querySelector('#submit-btn');
+    
